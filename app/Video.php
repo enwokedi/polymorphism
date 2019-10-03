bf9bca31d64f5223ea2bf79287c3ded1b70bac79
Emmanuel <emmanuel.nwokedi@gmail.com>
2019-10-03 09:50:30 +0100

    First commit

---
+++ app/Video.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    protected $guarded = [];
+
+    public function comments()
+
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    public function tags()
+
+    {
+        return $this->morphMany(Tag::class, 'taggable');
+    }
+}
